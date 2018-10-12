1b80018998a9162dbb57d562fc4713a9bf8da1a6
LENOVO-PC1\Markus <hansimglueck45>
2018-10-12 11:52:43 +0200

    Summe a+b hinzugefügt

--- Testprojectb2/src/TestClass.java
+++ Testprojectb2/src/TestClass.java
@@ -1,8 +1,12 @@
 
 public class TestClass {
+	
+	static int a = 5;
+	static int b = 10;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("Die Summe ist:"  + a+b);
 
 	}
 
